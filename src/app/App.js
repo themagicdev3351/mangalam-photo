eb37a20117ac7687f172c25a4512f2b7934c87c5
themagicdev3351 <themagicdev3351@gmail.com>
2024-08-02 16:16:12 +0530

    add arrow

--- src/app/App.js
+++ src/app/App.js
@@ -22,7 +22,7 @@ const ScrollToTop = withRouter(_ScrollToTop);
 export default function App() {
   return (
     <Router basename={process.env.PUBLIC_URL}>
-      {/* <div className="cursor__dot">
+      <div className="cursor__dot">
         <AnimatedCursor
           innerSize={15}
           outerSize={15}
@@ -31,7 +31,7 @@ export default function App() {
           innerScale={0.7}
           outerScale={5}
         />
-      </div> */}
+      </div>
       <ScrollToTop>
         <Headermain />
         <AppRoutes />
