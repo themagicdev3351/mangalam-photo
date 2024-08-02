98b7324549118581b93d59fba8aa71cfcb8aa404
themagicdev3351 <themagicdev3351@gmail.com>
2024-08-02 16:13:26 +0530

    first commit

---
+++ src/components/socialicons/index.js
@@ -0,0 +1,71 @@
+import React from "react";
+import "./style.css";
+import {
+  FaGithub,
+  FaTwitter,
+  FaFacebookF,
+  FaLinkedin,
+  FaYoutube,
+  FaTwitch,
+  FaInstagram
+} from "react-icons/fa";
+import { socialprofils } from "../../content_option";
+
+export const Socialicons = (params) => {
+  return (
+    <div className="stick_follow_icon">
+      <ul>
+        {socialprofils.twitter && (
+          <li>
+            <a target="_blank" href={socialprofils.twitter}>
+              <FaTwitter />
+            </a>
+          </li>
+        )}
+        {socialprofils.github && (
+          <li>
+            <a target="_blank" href={socialprofils.github}>
+              <FaGithub />
+            </a>
+          </li>
+        )}
+        {socialprofils.facebook && (
+          <li>
+            <a target="_blank" href={socialprofils.facebook}>
+              <FaFacebookF />
+            </a>
+          </li>
+        )}
+        {socialprofils.linkedin && (
+          <li>
+            <a target="_blank" href={socialprofils.linkedin}>
+              <FaLinkedin />
+            </a>
+          </li>
+        )}
+        {socialprofils.youtube && (
+          <li>
+            <a target="_blank" href={socialprofils.youtube}>
+              <FaYoutube />
+            </a>
+          </li>
+        )}
+        {socialprofils.instagram && (
+          <li>
+            <a target="_blank" href={socialprofils.instagram}>
+              <FaInstagram />
+            </a>
+          </li>
+        )}
+        {socialprofils.twitch && (
+          <li>
+            <a target="_blank" href={socialprofils.twitch}>
+              <FaTwitch />
+            </a>
+          </li>
+        )}
+      </ul>
+      <p>Follow Me</p>
+    </div>
+  );
+};
